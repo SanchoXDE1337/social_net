25391525d4934833ffcd72246738a5c81c1a44e6
Alexander Dolgov <sanhoxd@gmail.com>
2019-09-17 23:04:51 +0300

    Updates

---
+++ precache-manifest.c2d9be6b797a6f28a9dbeb11f2588aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83eee45ea2e006a6f0ad2584ff45f97b",
+    "url": "/social_net/index.html"
+  },
+  {
+    "revision": "01cb9ddbe15daac361f3",
+    "url": "/social_net/static/css/main.f59dbf67.chunk.css"
+  },
+  {
+    "revision": "11628869bd28430a8dd7",
+    "url": "/social_net/static/js/2.92b8cfd9.chunk.js"
+  },
+  {
+    "revision": "01cb9ddbe15daac361f3",
+    "url": "/social_net/static/js/main.6ffe3c15.chunk.js"
+  },
+  {
+    "revision": "a3c6cfcbbd603e3aead8",
+    "url": "/social_net/static/js/runtime~main.8bd986ea.js"
+  },
+  {
+    "revision": "8d0d6cf44ce424ba9107f1634d34fd52",
+    "url": "/social_net/static/media/logo.8d0d6cf4.jpg"
+  }
+]);
